9c9faf3a16cbcf8c105dc8fd6fa8c1a455fdf974
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-01 14:38:18 +0800

    Create sign7.js

---
+++ sign7.js
@@ -0,0 +1,37 @@
+
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
+const url = `https://api.theone.art/user/api/userCheckIn/save`;
+const method = `POST`;
+const headers = {
+'Accept-Encoding' : `gzip, deflate, br`,
+'Origin' : `https://www.theone.art`,
+'Connection' : `keep-alive`,
+'Accept' : `application/json, text/plain, */*`,
+'Referer' : `https://www.theone.art/`,
+'Host' : `api.theone.art`,
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzExMDg5NzczMSIsImlhdCI6MTY1MTM4Mzc5OSwiZXhwIjoxNjUxOTg4NTk5fQ.lKcIlkuhGOr9ugpis4G47mtvvFVlYTj5IMw8uzvHbYwLXGyTFdHOe9xeQLatsjBgqSRNeSioA7tig36zkrov2Q`,
+'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
+};
+const body = ``;
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+
+    $notify("D7", "Subtitle", response.body); // Success!
+    $done();
+}, reason => {
+    console.log(reason.error);
+    $done();
+});
