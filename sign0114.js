8ae3770f20c4e2779c025f883b0fede6a43bb41c
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-03 11:12:39 +0800

    Update sign0114.js

--- sign0114.js
+++ sign0114.js
@@ -29,7 +29,7 @@ const myRequest = {
 $task.fetch(myRequest).then(response => {
     console.log(response.statusCode + "\n\n" + response.body);
 
-    $notify("18658610114", "Subtitle", response.body); // Success!
+    $notify("W8", "18658610114", response.body); // Success!
     $done();
 }, reason => {
     console.log(reason.error);
