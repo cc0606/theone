bb90fea99da8fbed2bdc5f0743d795a1ec5df6ef
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-07 23:07:56 +0800

    Update z1.js

--- z1.js
+++ z1.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `Hm_lpvt_37e1f8a986a871fd2944042865a17e6a=1651857205; Hm_lvt_37e1f8a986a871fd2944042865a17e6a=1651857153; acw_tc=2f6fc12516518571505701807eedd56736e56abfea740cc87ef7cda09283dd; aliyungf_tc=58c54e529dd24d8cf93086ba5d0fff2b87021632eda100a100fb431d45422072`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTUyNzIwMjEwMCIsImlhdCI6MTY1MTg1MzI5NywiZXhwIjoxNjUyNDU4MDk3fQ.1AHLLn9Ri49hoddKI2UjXpZVc-H_AYeG1eUsmpTvJZOuDgOVB8pMJj_SfVLctRXiNzJIXTUAmRHJUMeLtQHIAA`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
