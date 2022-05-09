8da3d8395195154eae246255b5e6fa5105a5fb62
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-09 20:26:34 +0800

    Update sign7058.js

--- sign7058.js
+++ sign7058.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY1ODYzNzA1OCIsImlhdCI6MTY1MTM4NDQyMSwiZXhwIjoxNjUxOTg5MjIxfQ.S1gtavGoY2fCAKO1CppLsOP4XDMVTQbwTZ_o2BtjjHH8Tssu9vfOezO0Ot4OJUHpfzdOjU6290ERxa8qE82RGQ`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY1ODY5NjgyMiIsImlhdCI6MTY1MjA5ODU0NSwiZXhwIjoxNjUyNzAzMzQ1fQ.F5DuS8u-tRkH3jlWe6rRexEPx6HTbp00dyeO0oaBvirftaPFjf45hKmBhqSOQhzi4HmhZq7zPCu_8HRU7Nhpeg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
