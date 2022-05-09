4200024ea71c1253627b795089baa1739dd16764
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-09 20:31:15 +0800

    Update sign7.js

--- sign7.js
+++ sign7.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzExMDg5NzczMSIsImlhdCI6MTY1MTM4Mzc5OSwiZXhwIjoxNjUxOTg4NTk5fQ.lKcIlkuhGOr9ugpis4G47mtvvFVlYTj5IMw8uzvHbYwLXGyTFdHOe9xeQLatsjBgqSRNeSioA7tig36zkrov2Q`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzgwNTkzOTAwNiIsImlhdCI6MTY1MjA4NDQ2MywiZXhwIjoxNjUyNjg5MjYzfQ.dSiW3HC_ALp-VyADWZFn5dUaoK_LAJcZPoo7aX6xU-yvFl1-BnO007G1c_d_7a_x9kB2MP9efJXSG0QRhyoWpg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
