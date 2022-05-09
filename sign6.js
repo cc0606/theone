f35badcf142c73f33ca755d85da456308e8fcc6d
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-09 20:30:57 +0800

    Update sign6.js

--- sign6.js
+++ sign6.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTIwNTA0MDY5NyIsImlhdCI6MTY1MTM4MzcxMywiZXhwIjoxNjUxOTg4NTEzfQ.LFIzTcFRvxy--Wg95by2fASP-v6TyURcS4D_ZiWG-yHI8S6nRIO6s8yt1hG7vyjMMvqsRm9OooKKYfpeBeXrWg`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTIwNTA0MDY5NyIsImlhdCI6MTY1MjA4MTY1NywiZXhwIjoxNjUyNjg2NDU3fQ.MkGhi4OQMV7u6nOApRRcMlOoORZrBsKJ8gFNIylulGdv53g_gCAzMw-8YhrK4TKjCEniS494DdRcyLa6IsczVQ`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
