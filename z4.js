6bea674b5a53e96d5867f99340b35a2c59d7c9a6
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-16 12:08:32 +0800

    Update z4.js

--- z4.js
+++ z4.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTkyNzE0MjA0NCIsImlhdCI6MTY1MTg1Nzk1NSwiZXhwIjoxNjUyNDYyNzU1fQ.ZOrsso180KyJTYkUz7qPfzwPmSBQjQz5THPorqhFk4A5hFtq8_FmgJn3Uu45JYl68hkTl9aICmhE3IPjflEy9Q`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODA4NjQ2ODAzOSIsImlhdCI6MTY1MjU4ODYyOCwiZXhwIjoxNjUzMTkzNDI4fQ.tmpvuePFniWJpGCmjpzgYNNzH0IN1hmtZaebqIAs1YSh5kC4QkY80OAJeAkzkL7xqFK74JNEOOZ3jGkrckiQQw`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
