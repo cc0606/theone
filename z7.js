0bb3b7cfac3d9aca9f1cac10f3806bc237d97009
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-16 12:09:31 +0800

    Update z7.js

--- z7.js
+++ z7.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODA4NjQ2ODAzOSIsImlhdCI6MTY1MTg1NjU5NywiZXhwIjoxNjUyNDYxMzk3fQ.4M9D8IssmLAyHVRDTi7ptefceyRE6x396FITg2tg2uIp5EKX_JdlHlQ0Q0Asq9hUasgD81LZO11mmnW7ZUy6mA`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTg3MTQ5NDY0MCIsImlhdCI6MTY1MjU5MjI2OSwiZXhwIjoxNjUzMTk3MDY5fQ.PXIxz6Oho0qkHo8IbfqUmEBygMLZEFK47wpIvX_xsP0hr6_O0tw49ZorQul-KB3Aj5plhBb8Zw3GSyv4TwAfAg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
