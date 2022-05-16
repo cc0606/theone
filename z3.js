839e790b06bfeb9cad59a19be3fb6ba6e01156c7
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-16 12:08:13 +0800

    Update z3.js

--- z3.js
+++ z3.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzQzNjg1ODQxMSIsImlhdCI6MTY1MTg1NzIwNCwiZXhwIjoxNjUyNDYyMDA0fQ.nNwLvHwyrDgClTr95So05q_QKpeTPT5e-NFT-g1ROKCp8Lyb2qooVp47FPjYvZLPtd_QHMbL97Qf1EIVdzzBOQ`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzQzNjg1ODQxMSIsImlhdCI6MTY1MjU4ODQ5NywiZXhwIjoxNjUzMTkzMjk3fQ.MI17GTn5-UJNjARVR6MhK5K5JUsWKkv2viK6k0NBt7GoECuIlVzEdQiP6BvuG0fFohd6fbR9GtLd97Hwge31hg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
