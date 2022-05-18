7b79bdff338adf83d6ce1956d434a8dc19058ecc
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-18 18:14:17 +0800

    Update sign1537.js

--- sign1537.js
+++ sign1537.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzI2Mjk3MTUzNyIsImlhdCI6MTY1MjE2OTg1MCwiZXhwIjoxNjUyNzc0NjUwfQ.s0LzOgKViIelCSHQONEMbgXdpl9u9SxHz5GQ4OpOfKv8jVc0O5Iao9cAT_iysLkMmq3WS6G1I7Rbk3vMztIeZw`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzI2Mjk3MTUzNyIsImlhdCI6MTY1Mjg2NzQ4NSwiZXhwIjoxNjUzNDcyMjg1fQ.Vihrfecj1uFFY8vSZyUS6LNiPhtGN-SPD4oA3G5PLnFhN-yMRyWVWrC1hWheZCiACDfdQkVfkWpRL8GquQDY8A`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
