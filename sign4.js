a64c11665cbd299d2ecf834caf667b7d935cdcd6
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-18 15:31:32 +0800

    Update sign4.js

--- sign4.js
+++ sign4.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcxNTkxMDY5NyIsImlhdCI6MTY1MjA4MDE3MiwiZXhwIjoxNjUyNjg0OTcyfQ.K9XIzeh_ZdriELueNp0fX5I1HAEQW0UUSZeGI5_pVEArU8wGMIdN0vo_ngvrG5CbQ7JkTIB1PqH4pSCidH1C7g`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODc1MDExMDY5NyIsImlhdCI6MTY1MjgwMDUxMywiZXhwIjoxNjUzNDA1MzEzfQ.Pb39ZNWHWC8XZ8L5IM-MbpcjLiKlp8E7BbJxi58504iUlwCmcTb0hMo4gsTcDT5gLO2-fJaNwSDSPNB4YXCelg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
