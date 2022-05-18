09979cf486057851bffb65f8c839efbd25f4e2aa
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-18 17:18:43 +0800

    Update sign8676.js

--- sign8676.js
+++ sign8676.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODYyMTUyODY3NiIsImlhdCI6MTY1MjE2OTY5MiwiZXhwIjoxNjUyNzc0NDkyfQ.ffINph0aSADa5ZkOBAnCxth3b1W1yH3Vu0Yr4jHJvj1E4n3qIrzs7j0GkdmBn2B2bjy0duoFCMLVurFaotgUeQ`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODYyMTUyODY3NiIsImlhdCI6MTY1Mjg2NTMwOSwiZXhwIjoxNjUzNDcwMTA5fQ.Z3SU2teXVlhG8d9reZ5kBfDpa1Oond6ht0UxQ0-vFG5U1ohO_VrUz0aAwqj8vKXIoDYwUXZ2JYjYuyhn5Ry1TA`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
