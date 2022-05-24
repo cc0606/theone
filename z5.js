64c49cfb67f32d03c60dfa8851b812273dcd0c6b
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-24 12:35:50 +0800

    Update z5.js

--- z5.js
+++ z5.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTY3MTg2OTgzMCIsImlhdCI6MTY1MjU4ODg0NywiZXhwIjoxNjUzMTkzNjQ3fQ.Hc-N-WNGfa9AQGR5gSC9M8FG_XojDl3syXDJ09oRPkQQ4WgrsENM_gAK_BaHOya7f5pjAQFyB1SxLQQLC_on4g`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTkyNzE0MjA0NCIsImlhdCI6MTY1MzMyMDk3NCwiZXhwIjoxNjUzOTI1Nzc0fQ.5G0QOUddD9maEXTxrQt10azVJTDhb7PVb30-F4EcmIUUcooQaypCCf6lMJoVBOWHxiEyk5_zavZ8fSZ_JbuipQ`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
