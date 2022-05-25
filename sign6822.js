d66af02758c8e25a3ce8f2f7995bff99522cc835
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-25 21:08:00 +0800

    Update sign6822.js

--- sign6822.js
+++ sign6822.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY1ODY5NjgyMiIsImlhdCI6MTY1MjgwMTA3OCwiZXhwIjoxNjUzNDA1ODc4fQ.cxm00W-dibfPz61LHnyAYmVZ-sgO7vKPQLNpUd9JfNW09Px45CuSHq2eaQveOiTruMtIYJMG1pHB0c61RXR5Ww`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY1ODY5NjgyMiIsImlhdCI6MTY1MzQ4MzkxMywiZXhwIjoxNjU0MDg4NzEzfQ.2RnCSh5DiXl5EJIDW97LzuAHo09Evj8PnsolreNV2_MaZ-RTwiXYYF29MA_felvbQ8yH_G5B8z1iNVGWIYd7qw`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
