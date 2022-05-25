ec40f4c88c141118b75f929a20d53818b412a7cb
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-25 21:11:31 +0800

    Update sign0261.js

--- sign0261.js
+++ sign0261.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTM5NzQ3MDI2MSIsImlhdCI6MTY1MjgwMTI4NSwiZXhwIjoxNjUzNDA2MDg1fQ.CnWpyuAAatsy9CpQoScZnyT_VwvdsZqvFOpGRA-RBRkrjScr6Hl--AaNrbOScONIKsH1EDR_FApDgOTpK-4lTA`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTM5NzQ3MDI2MSIsImlhdCI6MTY1MzQ4NDI1MywiZXhwIjoxNjU0MDg5MDUzfQ.oPXCEXSpkgDgQUWFcqOqaYV1sLvJYewnGxpHrUzMUTA9xhbeNStLztMnUVsIyCGELuXsCVQe8XkRQxwWO0UBsw`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
