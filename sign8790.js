5f627c61d625fadd34d8c611545491e22f36011f
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-25 21:16:42 +0800

    Update sign8790.js

--- sign8790.js
+++ sign8790.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEwNjAxODc5MCIsImlhdCI6MTY1MjgwMTYyOCwiZXhwIjoxNjUzNDA2NDI4fQ.OO2gM12y-NRD-nW_IZGlnxO7shdONAhA1eS4L23_wCngmn3RwSU5xIOGxMOajAWwDSxJ0QfexV4Fx9d5VHccqQ`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEwNjAxODc5MCIsImlhdCI6MTY1MzQ4NDU2OSwiZXhwIjoxNjU0MDg5MzY5fQ.lO6SYSpU93vdnuqT7VyiuABSX6ulhwGTR5DfIpwh9m2LaIfrhsynt_6__Y4uCe6qoMNB1M4Ic5tAyAkwth9Ftg`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
