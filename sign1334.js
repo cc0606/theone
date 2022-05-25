30545c40474a9b6d3c0ded2465fc359ac22e4c29
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-25 21:13:25 +0800

    Update sign1334.js

--- sign1334.js
+++ sign1334.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTgyNDAwMTMzNCIsImlhdCI6MTY1MjgwMTQwOCwiZXhwIjoxNjUzNDA2MjA4fQ._Ras8CfC12UF-UcP3BT36xUmYwWzWzTX5ArxbHQejmWJOVAzF8ZlRWIyEj56ucsvPRJITb9cU0rLLWiPlV1sng`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTgyNDAwMTMzNCIsImlhdCI6MTY1MzQ4NDM2MSwiZXhwIjoxNjU0MDg5MTYxfQ.wcTbyr-kqBVnSBoU-f_ggE6JVjvaeP5IJNiIZH8B0DwgZNf1bs7dI6GhRiXEH8wer3tBvp8mBKD4PLBI6YNV7A`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
