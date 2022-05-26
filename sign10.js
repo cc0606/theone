d3111eaaf646f3dbe98c6d759d897019e37f0c11
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-26 12:09:07 +0800

    Update sign10.js

--- sign10.js
+++ sign10.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY0OTcwMDcyOSIsImlhdCI6MTY1Mjc4MzYzMywiZXhwIjoxNjUzMzg4NDMzfQ._T6GatfxHslb9DFAPbkwkVmoLoH2iJb7DwWTHXqNZcsrruiF4BlkY7wj3S7OoTt9P2iw65YOOtn4nr8xrz9T7Q`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY0OTcwMDcyOSIsImlhdCI6MTY1MzQ5MDM2OCwiZXhwIjoxNjU0MDk1MTY4fQ.GfXXc_icSMC-oCR7LkqcX54GFUIDBmniYdpO73biDpyw2lj5XdV_YorlE4U0s4x19r89ueCD3INpasFs8tsQgQ`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
