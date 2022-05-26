e1e7e2ff6300cc1a62308715315090118ad7e61b
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-26 12:08:27 +0800

    Update sign8.js

--- sign8.js
+++ sign8.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODE1OTE5NzEwMSIsImlhdCI6MTY1MjgwMjIyOSwiZXhwIjoxNjUzNDA3MDI5fQ.IrFPNIGLIPmnVAwiG2I9Kp5xW4ljF2qGHIVj9qmZcLhE4bs4dTWup6D2H6mLgPzUFr5uYxbCni8Kq-_-g2qb5w`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzg0MjUwMTA4NyIsImlhdCI6MTY1MzQ5MTA4NywiZXhwIjoxNjU0MDk1ODg3fQ._BZychldMg-ije6LvHQC6QJ97iXx1_2pTZtt_LITGnXyecbYumO8Coy6_t9m52Iqh9EGFiSwIzdb__V8dUTjog`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
